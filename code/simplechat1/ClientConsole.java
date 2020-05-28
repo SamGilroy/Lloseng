ed05e505148ac98c4aa05d807275359d5e3ce8b3
SamGilroy <65417959+SamGilroy@users.noreply.github.com>
2020-05-28 19:13:47 -0400

    added loginID function

--- code/simplechat1/ClientConsole.java
+++ code/simplechat1/ClientConsole.java
@@ -41,11 +41,11 @@ public class ClientConsole implements ChatIF
    * @param host The host to connect to.
    * @param port The port to connect on.
    */
-  public ClientConsole(String host, int port) 
+  public ClientConsole(String loginID, String host, int port) 
   {
     try 
     {
-      client= new ChatClient(host, port, this);
+      client= new ChatClient(loginID, host, port, this);
     } 
     catch(IOException exception) 
     {
@@ -106,10 +106,19 @@ public class ClientConsole implements ChatIF
   {
     String host = "";
     int port = 0;  //The port number
-
+    String loginID = "";
     try
     {
-      host = args[0];
+      loginID = args[0];
+    }
+    catch(ArrayIndexOutOfBoundsException e)
+    {
+      System.out.println("No Login ID provided, exiting client.");
+      System.exit(0);
+    }
+    try
+    {
+      host = args[1];
     }
     catch(ArrayIndexOutOfBoundsException e)
     {
@@ -118,7 +127,7 @@ public class ClientConsole implements ChatIF
 
     try
     {
-      port = Integer.parseInt(args[1]);
+      port = Integer.parseInt(args[2]);
     }
     catch(ArrayIndexOutOfBoundsException e)
     {
@@ -126,7 +135,7 @@ public class ClientConsole implements ChatIF
     }
 
     
-    ClientConsole chat= new ClientConsole(host, port);
+    ClientConsole chat= new ClientConsole(loginID, host, port);
     chat.accept();  //Wait for console data
   }
 }
